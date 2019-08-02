298139e4b8a2360ef85f0c2ec56eb8911d25a1e5
edjo93 <eddas.carrasco@unitec.edu>
2019-08-02 15:10:13 -0600

    class Animal added

---
+++ Animal.cpp
@@ -0,0 +1,11 @@
+#ifndef ANIMAL_CPP
+#define ANIMAL_CPP
+	#include"Patas.cpp"
+	#include"Pelaje.cpp"
+	#include"Ojos.cpp"
+	#include"Orejas.cpp"
+	#include"Cola.cpp"
+	class Animal{
+		
+	};
+#endif
